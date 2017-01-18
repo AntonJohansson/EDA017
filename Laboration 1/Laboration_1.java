49a889332a0c8d36aac13c0af545a016d8f356f9
Anton Johansson <an2178jo-s@falk-11.student.lth.se>
2017-01-18 15:34:29 +0100

    Added lth workspace

---
+++ Laboration 1/Laboration_1.java
@@ -0,0 +1,4 @@
+public class Laboration_1{
+	public static void main(String[] args){
+	}
+}
---
+++ ext/pt-workspace/Lab07/src/pair/PairSequence.java
@@ -0,0 +1,20 @@
+package pair;
+
+public class PairSequence {
+
+	/** Skapar en sekvens av alla talpar (a,b) sådana att
+	    0 <= a < rows  och  0 <= b < cols */
+	public PairSequence(int rows, int cols) {
+	}
+
+	/** Undersöker om det finns fler par i sekvensen. */
+	public boolean more() {
+		return false;
+	}
+
+	/** Hämtar ett slumpmässigt valt talpar ur sekvensen. Sekvensen
+    	blir ett element kortare. Om sekvensen är tom returneras null. */
+	public Pair pick() {
+		return null;
+	}
+}
