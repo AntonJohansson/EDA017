627adf4198df624c1a6c53813de4576bc80769b5
Anton Johansson <an2178jo-s@alfa-8.student.lth.se>
2017-02-16 17:07:56 +0100

    Redovisat lab 2

--- Matlab/Lab 1/Lab1.m
+++ Matlab/Lab 1/Lab1.m
@@ -1,16 +1,53 @@
-%% Komplexa tal
- 
+%% D2
+
+pi
+sqrt(3)
+sqrt(-3)
+sqrt(i)
+exp(1)^(pi*i)
+
+%% D3
+
+z = sqrt(i);
+abs(z)
+angle(z)
+
+z = exp(1)^(pi*i);
+real(z)
+imag(z)
+
+
+%% D4
+
 a = 3 + 2i;
 b = 5 - 1i;
 x = a * b
 
-%% Funktioner
+%% D5
 
 g = @(x) exp(1).^(-0.1*x) .* cos(x);
-h1 = @(x) exp(1).^(-0.1*x) .* cos(x) - 0.1*exp(1).^(-0.1*x).*sin(x);
-h2 = @(x) -0.1*exp(1).^(-0.1*x) .* cos(x) - exp(1).^(-0.1*x).*sin(x);
-h3 = @(x) -0.1*exp(1).^(-0.1*x) .* cos(x) - 0.1*exp(1).^(-0.1*x).*sin(x);
+g(0)
+g(2)
 
+%% D7
+
+g = @(x) exp(1).^(-0.1*x) .* cos(x);
+
+x = linspace(0, 10);
+y = g(x);
+
+plot(x, y);
+
+%% D8
+
+h = 1e-6;
+deriv = @(f, x) (f(x + h) - f(x))./h;
+
+error = deriv(@sin, 0) - cos(0)
+
+%% D9
+
+g = @(x) exp(1).^(-0.1*x) .* cos(x);
 x = linspace(0, 10);
 y = g(x);
 
@@ -24,3 +61,29 @@ plot(x, deriv(g, x), 'r');
 title('Funktionen och dess derivata');
 legend('funktion', 'derivata');
 print('funktionsbild', '-dpng');
+
+
+%% D10
+
+g = @(x) exp(1).^(-0.1*x) .* cos(x);
+x = linspace(0, 10);
+
+h = 1e-6;
+deriv = @(f, x) (f(x + h) - f(x))./h;
+
+h1 = @(x) exp(1).^(-0.1*x) .* cos(x) - 0.1*exp(1).^(-0.1*x).*sin(x);
+h2 = @(x) -0.1*exp(1).^(-0.1*x) .* cos(x) - exp(1).^(-0.1*x).*sin(x);
+h3 = @(x) -0.1*exp(1).^(-0.1*x) .* cos(x) - 0.1*exp(1).^(-0.1*x).*sin(x);
+
+y0 = deriv(g, x);
+y1 = h1(x);
+y2 = h2(x);
+y3 = h3(x);
+
+dy1 = y1 - y0;
+dy2 = y2 - y0;
+dy3 = y3 - y0;
+
+norm(dy1)
+norm(dy2)
+norm(dy3)
--- Matlab/Lab 2/Lab2.m
+++ Matlab/Lab 2/Lab2.m
@@ -1,3 +1,9 @@
+%% D1
+
+p = [1 3 -87 -307 822 2160];
+x = roots(p)
+y = polyval(p, x)
+
 %% D2
 
 p = poly([-5 -4 2 8]);
@@ -6,14 +12,37 @@ x = linspace(-50, 50);
 plot(x, polyval(p, x));
 axis([-50, 50, -750, 750]);
 
+%% D3
+
+f = @(t) 4*sin(3*t) + 3*exp(1).^(-0.5*t);
+S = integral(f, 1.4, 3.1)
+
 %% D4
 
 g = @(x) cos(exp(1).^x)./(1 - x);
 min = fminbnd(g, 2, 3);
-fprintf('Min: %d\n', min);
+y = g(min);
+
+fprintf('Min: x = %d\ng(x) = %d\n', min, y);
 
 x = linspace(2, 3);
 plot(x, g(x));
+grid on;
 
-%% D7
+%% D5
 
+h = @(x) 3*exp(1).^(-x.^2) - 9*x.^2 + x;
+g = @(x) -h(x);
+
+fplot(h, [-10 10]);
+
+x_roots = [fzero(h, 0) fzero(h, 0.2)]
+y_roots = g(x_roots)
+
+max = fminbnd(g, -50, 50)
+ymax = h(max)
+
+%% D6
+
+rows = 7;
+M = eye(rows) + 5*diag(ones(rows-1, 1), 1) + 5*diag(ones(rows-1, 1), -1)
