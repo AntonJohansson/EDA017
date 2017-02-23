1c5a2590431c68c836543f3d4e19f936a61b2c59
Anton Johansson <an2178jo-s@alfa-8.student.lth.se>
2017-02-23 17:13:12 +0100

    Påbörjat CardReadertest

--- ext/pt-workspace/Lab07/src/cardreader/CardReaderTest.java
+++ ext/pt-workspace/Lab07/src/cardreader/CardReaderTest.java
@@ -9,7 +9,15 @@ public class CardReaderTest {
 	
 	public void testUserTable(){
 		UserTable utable = new UserTable();
+		User u;
 		
-		if (utable.find(24073))
+		if ((u = utable.find(24073)) != null)System.out.printf("Kort 24073 OK: %s\n", u.toString());
+		else System.out.println("Fel: Kort 24073 gav ingen användare!");
+		
+		if ((u = utable.find(24074)) != null)System.out.printf("Kort 24074 OK: %s\n", u.toString());
+		else System.out.println("Fel: Kort 24074 gav ingen användare!");
+		
+		if ((u = utable.findByName("Jens Holmgren")) != null)System.out.printf("Användaren Jens Holmgren OK: %s\n", u.toString());
+		else System.out.println("Fel: Användaren Jens Holmgren hittades inte");
 	}
 }
